cbd489b59aa068551695fd5cb71b3c46a28c4fd2
Sorongos <dosora9@gmail.com>
2023-02-26 14:22:46 +0900

    setContentView에 binding.root
    넣어줘야지 칩그룹이 보임

--- app/src/main/java/com/sorongos/vocabook/AddActivity.kt
+++ app/src/main/java/com/sorongos/vocabook/AddActivity.kt
@@ -10,26 +10,29 @@ class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_add)
+        setContentView(binding.root)
 
         initViews()
+        binding.addButton.setOnClickListener {
+
+        }
     }
 
-    private fun initViews(){
+    private fun initViews() {
         val types = listOf(
-            "명사","동사","대명사","형용사",
-            "부사","감탄사","전치사","점속사"
+            "명사", "동사", "대명사", "형용사",
+            "부사", "감탄사", "전치사", "점속사"
         )
-        binding.typeChipGroup.apply{
+        binding.typeChipGroup.apply {
             //리스트의 아이템을 칩으로 변환
-            types.forEach{ text ->
+            types.forEach { text ->
                 addView(createChip(text))
             }
         }
     }
 
     /**칩 생성, 칩 그룹 안에*/
-    private fun createChip(text: String) : Chip{
+    private fun createChip(text: String): Chip {
         return Chip(this).apply {
             setText(text)
             isCheckable = true
